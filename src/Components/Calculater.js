91b0582a5b874c339bbd9779cffd2fd0a0687321
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-05-26 17:30:39 +0300

    Style the Calculater App by adding css properties to Calculater.css

--- src/Components/Calculater.js
+++ src/Components/Calculater.js
@@ -1,25 +1,29 @@
+import './Calculater.css'
+
+
 const Calculater = () => (
-  <section >
-      <p>0</p>
+  <section className='calculater_section' >
+      <p className='result'>0</p>
       <button>AC</button>
       <button>+/-</button>
-      <button>%</button>`
-      <button>÷</button>
+      <button>%</button>
+      <button className='operator'>÷</button>
+      
       <button>7</button>
       <button>8</button>
       <button>9</button>
-       <button>x</button>
+       <button className='operator'>x</button>
        <button>4</button>
        <button>5</button>
        <button>6</button>
-       <button>-</button>
+       <button className='operator'>-</button>
        <button>1</button>
        <button>2</button>
        <button>3</button>
-       <button>+</button>
-       <button>0</button>
+       <button className='operator'>+</button>
+       <button className='zero'>0</button>
        <button>.</button>
-       <button>=</button>
+       <button className='operator'>=</button>
 
 
 
