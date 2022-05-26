80d8cf68789f82806b49eb74f95a3807c15e4736
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-05-26 17:10:48 +0300

    Add Components file and Calculater.js file and the markup for calculater app

--- src/Components/Calculater.js
+++ src/Components/Calculater.js
@@ -1,8 +1,29 @@
 const Calculater = () => (
-  <div>
-    <h1>Hello from Calculater</h1>
+  <section >
+      <p>0</p>
+      <button>AC</button>
+      <button>+/-</button>
+      <button>%</button>`
+      <button>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+       <button>x</button>
+       <button>4</button>
+       <button>5</button>
+       <button>6</button>
+       <button>-</button>
+       <button>1</button>
+       <button>2</button>
+       <button>3</button>
+       <button>+</button>
+       <button>0</button>
+       <button>.</button>
+       <button>=</button>
 
-  </div>
+
+
+  </section>
 );
 
 export default Calculater;
