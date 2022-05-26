c3b025b31a37ab3bebb60a9049ce99df76971b80
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-05-26 17:40:05 +0300

    Fix linter errors

--- src/Components/Calculater.js
+++ src/Components/Calculater.js
@@ -1,31 +1,28 @@
-import './Calculater.css'
-
+import './Calculater.css';
 
 const Calculater = () => (
-  <section className='calculater_section' >
-      <p className='result'>0</p>
-      <button>AC</button>
-      <button>+/-</button>
-      <button>%</button>
-      <button className='operator'>÷</button>
-      
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-       <button className='operator'>x</button>
-       <button>4</button>
-       <button>5</button>
-       <button>6</button>
-       <button className='operator'>-</button>
-       <button>1</button>
-       <button>2</button>
-       <button>3</button>
-       <button className='operator'>+</button>
-       <button className='zero'>0</button>
-       <button>.</button>
-       <button className='operator'>=</button>
+  <section className="calculater_section">
+    <p className="result">0</p>
+    <button>AC</button>
+    <button>+/-</button>
+    <button>%</button>
+    <button className="operator">÷</button>
 
-
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button className="operator">x</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button className="operator">-</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button className="operator">+</button>
+    <button className="zero">0</button>
+    <button>.</button>
+    <button className="operator">=</button>
 
   </section>
 );
