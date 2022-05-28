c2d444a711f27e609939b15d9195342f46e2cacf
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-05-28 19:46:36 +0300

    Fix eslint errors

--- src/Components/Calculater.js
+++ src/Components/Calculater.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Calculater.css';
 import calculate from '../logic/calculate';
 
-
 class Calculater extends React.Component {
   constructor(props) {
     super(props);
@@ -10,42 +9,44 @@ class Calculater extends React.Component {
       total: 0,
       next: null,
       operation: null,
-    }; 
+    };
     this.clickButton = this.clickButton.bind(this);
   }
 
   clickButton(e) {
     this.setState((state) => calculate(state, e.target.name));
   }
+
   render() {
-    const {total,operation,next} = this.state;
+    const { total, operation, next } = this.state;
     return (
-      <section className="calculater_section"><p className="result">
-      {total}
-      {operation}
-      {next}
-    </p>
-    <button type="button" name="AC" onClick={this.clickButton}>AC</button>
-    <button type="button" name="+/-" onClick={this.clickButton}>+/-</button>
-    <button type="button" name="%" onClick={this.clickButton}>%</button>
-    <button type="button" className="operator" name="÷" onClick={this.clickButton}>÷</button>
-    <button type="button" name="7" onClick={this.clickButton}>7</button>
-    <button type="button" name="8" onClick={this.clickButton}>8</button>
-    <button type="button" name="9" onClick={this.clickButton}>9</button>
-    <button type="button" className="operator" name="x" onClick={this.clickButton}>x</button>
-    <button type="button" name="4" onClick={this.clickButton}>4</button>
-    <button type="button" name="5" onClick={this.clickButton}>5</button>
-    <button type="button" name="6" onClick={this.clickButton}>6</button>
-    <button type="button" className="operator" name="-" onClick={this.clickButton}>-</button>
-    <button type="button" name="1" onClick={this.clickButton}>1</button>
-    <button type="button" name="2" onClick={this.clickButton}>2</button>
-    <button type="button" name="3" onClick={this.clickButton}>3</button>
-    <button type="button" className="operator" name="+" onClick={this.clickButton}>+</button>
-    <button type="button" className="zero" name="0" onClick={this.clickButton}>0</button>
-    <button type="button" name="." onClick={this.clickButton}>.</button>
-    <button type="button" className="operator" name="=" onClick={this.clickButton}>=</button>
-  </section>
-);
-}
+      <section className="calculater_section">
+        <p className="result">
+          {total}
+          {operation}
+          {next}
+        </p>
+        <button type="button" name="AC" onClick={this.clickButton}>AC</button>
+        <button type="button" name="+/-" onClick={this.clickButton}>+/-</button>
+        <button type="button" name="%" onClick={this.clickButton}>%</button>
+        <button type="button" className="operator" name="÷" onClick={this.clickButton}>÷</button>
+        <button type="button" name="7" onClick={this.clickButton}>7</button>
+        <button type="button" name="8" onClick={this.clickButton}>8</button>
+        <button type="button" name="9" onClick={this.clickButton}>9</button>
+        <button type="button" className="operator" name="x" onClick={this.clickButton}>x</button>
+        <button type="button" name="4" onClick={this.clickButton}>4</button>
+        <button type="button" name="5" onClick={this.clickButton}>5</button>
+        <button type="button" name="6" onClick={this.clickButton}>6</button>
+        <button type="button" className="operator" name="-" onClick={this.clickButton}>-</button>
+        <button type="button" name="1" onClick={this.clickButton}>1</button>
+        <button type="button" name="2" onClick={this.clickButton}>2</button>
+        <button type="button" name="3" onClick={this.clickButton}>3</button>
+        <button type="button" className="operator" name="+" onClick={this.clickButton}>+</button>
+        <button type="button" className="zero" name="0" onClick={this.clickButton}>0</button>
+        <button type="button" name="." onClick={this.clickButton}>.</button>
+        <button type="button" className="operator" name="=" onClick={this.clickButton}>=</button>
+      </section>
+    );
+  }
 }
 export default Calculater;
