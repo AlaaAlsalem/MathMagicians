7a78b7438a28f7958a90b2c26c8cedd35bf6c9ab
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-02 05:46:08 +0300

    Add Calculator component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Calculater from './Components/Calculater';
+import Calculator from './pages/Calculator';
 
 class App extends React.PureComponent {
   render() {
     return (
       <div className="App">
-        <h1>
-          Hello from App
-        </h1>
-        <Calculater />
+        <Calculator />
 
       </div>
     );
---
+++ src/pages/Calculator.js
@@ -0,0 +1,18 @@
+import Calculater from '../Components/Calculater'
+
+const Calculator = () => {
+
+    return (
+        <div>
+            <div>
+                <h1>Math Magicians </h1>
+                <h3> Let's do some Math</h3>
+            </div>
+            <div>
+                <Calculater/>
+            </div>
+        </div>
+    )
+}
+
+export default Calculator
