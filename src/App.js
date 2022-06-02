3c200d32e74375228b0008641eeae90551c8aca3
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-02 07:44:49 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
 import Calculator from './pages/Calculator';
+import { Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Navbar from './pages/Navbar';
+
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div className="App">
-        <Calculator />
+      <>
+        <Navbar/>
+        <Switch>
+          <Route exact path='/'>
+            <div className="App">
+              <Home />
 
-      </div>
+            </div>
+          </Route>
+          <Route exact path='/calculator'>
+            <Calculator />
+          </Route>
+          <Route exact path='/quote'>
+            <Quote />
+          </Route>
+        </Switch>
+      </>
     );
   }
 }
